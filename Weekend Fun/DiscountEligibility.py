c39da8b254bdc903801077706637792ade047b07
Gannix <xgannix@gmail.com>
2025-12-03 12:13:01 +0100

    updated

--- Weekend Fun/DiscountEligibility.py
+++ Weekend Fun/DiscountEligibility.py
@@ -2,8 +2,8 @@
 total_bill = float(input("Input your total bill: "))
 is_member = input("Are you a member? Yes/No")
 
-if (total_bill >= 1000 and (is_member == "Yes" or is_member == "yes")):
-	print("Your bill after 10% discount is", round((0.9 * total_bill)))
+if (total_bill >= 1000 and (is_member == "Yes" or is_member == "yes" or is_member == "YES")):
+	print("Your bill after 10% discount is", round((0.9 * total_bill), 2))
 elif (total_bill >= 1000 and (is_member == "No" or is_member == "no")):
 	print("Your bill after 5% discount is", round((0.95 * total_bill), 2))
 else:
