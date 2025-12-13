1d762f3262e147a705572eb899b583a27cc7ba42
Gannix <xgannix@gmail.com>
2025-12-13 12:20:57 +0100

    christma song task

---
+++ Christmas Song/christmas_song.py
@@ -0,0 +1,61 @@
+
+for num in range (1, 13):
+
+	match (num):
+		case 1:
+			print("On the first day of christmas")
+		case 2:
+			print("On the first day of christmas")
+		case 3:
+			print("On the third day of christmas")
+		case 4:
+			print("On the fourth day of christmas")
+		case 5:
+			print("On the fifth day of christmas")
+		case 6:
+			print("On the sixth day of christmas")
+		case 7:
+			print("On the seventh day of christmas")
+		case 8:
+			print("On the eighth day of christmas")
+		case 9:
+			print("On the ninth day of christmas")
+		case 10:
+			print("On the tenth day of christmas")
+		case 11:
+			print("On the eleventh day of christmas")
+		case 12:
+			print("On the twelfth day of christmas")
+	
+	print("My true love gave to me")
+
+	for inner_number in range(num, 0, -1):
+		match (inner_number):
+
+			case 1:
+				print("A partridge in a peer tree")
+			case 2:
+				print("Two turtle dove and")
+			case 3:
+				print("Three French hens")
+			case 4:
+				print("Four calling birds")
+			case 5:
+				print("Five golden rings")
+			case 6:
+				print("Six geese a-laying")
+			case 7:
+				print("Seven swans a-swimming")
+			case 8:
+				print("Eight maids a-milking")
+			case 9:
+				print("Nine ladies dancing")
+			case 10:
+				print("Ten lords a-leaping")
+			case 11:
+				print("Eleven pipers piping")
+			case 12:
+				print("Twelve drummers drumming")
+
+	print()
+
